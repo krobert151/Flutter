0f17cd26673297a558731d9862c6ba31af3ae23b
Roberto <robertorebolledo151@gmail.com>
2024-01-10 14:29:28 +0100

    to home

--- login_molon/lib/screen/login.dart
+++ login_molon/lib/screen/login.dart
@@ -1,29 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:login_molon/widget/log_in.dart';
-import 'package:login_molon/widget/logo.dart';
-import 'package:login_molon/widget/sign_up.dart';
-import 'package:login_molon/widget/terms_of_use.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const blanquito = TextStyle(
+      color: Color.fromARGB(199, 255, 255, 255),
+      fontWeight: FontWeight.bold,
+    );
+
+    const negrito = TextStyle(
+        decoration: TextDecoration.none,
+        color: Colors.black87,
+        fontWeight: FontWeight.normal);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 230, 46, 22),
       ),
       body: Container(
+        width: double.infinity,
         alignment: Alignment.center,
         color: const Color.fromARGB(255, 230, 46, 22),
         child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Logo(),
-          Register(enabled: true),
-          Login(enabled: true),
-          TermsOfUse()
-        ]),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      Image(image: AssetImage('media/path_logo.png')),
+                      Text(
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white54),
+                          'Beautifull, Private Sharing')
+                    ],
+                  )),
+              Expanded(flex: 1, child: Divider()),
+              Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                      onPressed: null,
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.white54),
+                          foregroundColor: MaterialStatePropertyAll(Colors.red),
+                          fixedSize: MaterialStatePropertyAll(Size(250, 60)),
+                          side: MaterialStatePropertyAll(BorderSide(
+                              color: Color.fromARGB(199, 255, 255, 255),
+                              width: 2)),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))))),
+                      child: Text('Sign Up'))),
+              Expanded(
+                  flex: 1,
+                  child: OutlinedButton(
+                      onPressed: null,
+                      style: ButtonStyle(
+                          fixedSize: MaterialStatePropertyAll(Size(250, 60)),
+                          side: MaterialStatePropertyAll(BorderSide(
+                              color: Color.fromARGB(199, 255, 255, 255),
+                              width: 2)),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))))),
+                      child: Text('loggin'))),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Text("By ussing Path, you agree to Path's"),
+                      Text.rich(TextSpan(
+                          text: 'Terms of Use',
+                          style: blanquito,
+                          children: <TextSpan>[
+                            TextSpan(text: ' And ', style: negrito),
+                            TextSpan(text: 'Privacity Policy', style: blanquito)
+                          ]))
+                    ],
+                  ))
+            ]),
       ),
     );
   }
--- login_molon/lib/widget/log_in.dart
+++ login_molon/lib/widget/log_in.dart
@@ -11,35 +11,26 @@ class Login extends StatelessWidget {
       color: const Color.fromARGB(255, 230, 46, 22),
       child: Column(children: [
         Container(
-          margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
           child: const Text(
-            style: TextStyle(
-              color: Color.fromARGB(199, 255, 255, 255),
-            ), 'Already have a Path Account)'),
+              style: TextStyle(
+                color: Color.fromARGB(199, 255, 255, 255),
+              ),
+              'Already have a Path Account'),
         ),
-        
         TextButton(
           onPressed: onPressed,
           style: TextButton.styleFrom(
-            foregroundColor: const Color.fromARGB(199, 255, 255, 255),
-            side: const BorderSide(
-              color: Color.fromARGB(199, 255, 255, 255),
-              width: 2
-              ),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(10)
-                )
-              ),
-            fixedSize: const Size(250, 60),        
-            textStyle:const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 20
-            )
-        ),
+              foregroundColor: const Color.fromARGB(199, 255, 255, 255),
+              side: const BorderSide(
+                  color: Color.fromARGB(199, 255, 255, 255), width: 2),
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              fixedSize: const Size(250, 60),
+              textStyle:
+                  const TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
           child: const Text('Log In'),
-          )
-        
+        )
       ]),
     );
   }
